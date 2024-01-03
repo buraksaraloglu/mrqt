9e7e3582b0d126aa26ef041ee75eddad1a6d35a1
Burak Saraloglu <buraksaraloglu1@gmail.com>
2024-01-03 14:57:38 +0100

    feat: facebook integration page

--- app/(app)/app/integrations/facebook/page.tsx
+++ app/(app)/app/integrations/facebook/page.tsx
@@ -6,13 +6,19 @@ import {
 } from "@/services/facebook/service/ad-account";
 import { AdAccount } from "@/services/facebook/types";
 import { FacebookAdAccount } from "@prisma/client";
+import Balancer from "react-wrap-balancer";
 
 import { siteConfig } from "@/config/site";
 import { requireUser } from "@/lib/auth";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import { Typography } from "@/components/ui/typography";
 import { PageContent, PageHeader } from "@/components/page";
-import { Icons } from "@/components/shared/icons";
 
 import { IntegrationNav, NavLink } from "../navigation";
 import { FacebookAdAccountsTable } from "./table";
@@ -35,14 +41,37 @@ export default async function FacebookIntegrationPage() {
           </>
         }
       />
-      <PageContent>
+      <PageContent className="space-y-4">
         <IntegrationNav />
-        {fbAdAccounts && (
-          <FacebookAdAccountsTable
-            fbAdAccounts={fbAdAccounts}
-            userAdAccounts={userFbAdAccounts}
-          />
-        )}
+
+        <div className="space-y-4">
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="custom-conversions">
+              <AccordionTrigger iconPosition="left">
+                <Typography variant="h3">Custom conversions</Typography>
+              </AccordionTrigger>
+              <AccordionContent innerClassName="space-y-2">
+                <div className="max-w-2xl">
+                  <Balancer>
+                    <Typography variant="p" className="text-muted-foreground">
+                      If you have your custom conversions set up on Facebook,
+                      you can use them in {siteConfig.name}. Click Sync to load
+                      your custom conversions.
+                    </Typography>
+                  </Balancer>
+                </div>
+                <Button variant="secondary">Sync conversions</Button>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          {fbAdAccounts && (
+            <FacebookAdAccountsTable
+              fbAdAccounts={fbAdAccounts}
+              userAdAccounts={userFbAdAccounts}
+            />
+          )}
+        </div>
       </PageContent>
     </>
   );
--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as AccordionPrimitive from "@radix-ui/react-accordion"
-import { ChevronDown } from "lucide-react"
+import * as React from "react";
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import { ChevronDown } from "lucide-react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const Accordion = AccordionPrimitive.Root
+const Accordion = AccordionPrimitive.Root;
 
 const AccordionItem = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Item>,
@@ -17,44 +17,55 @@ const AccordionItem = React.forwardRef<
     className={cn("border-b", className)}
     {...props}
   />
-))
-AccordionItem.displayName = "AccordionItem"
+));
+AccordionItem.displayName = "AccordionItem";
 
 const AccordionTrigger = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger> & {
+    iconPosition?: "left" | "right";
+  }
+>(({ className, children, iconPosition = "right", ...props }, ref) => (
   <AccordionPrimitive.Header className="flex">
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
         "flex flex-1 items-center justify-between py-4 text-left font-medium transition-all hover:underline max-sm:text-sm [&[data-state=open]>svg]:rotate-180",
-        className
+        className,
       )}
       {...props}
     >
-      {children}
-      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      <div className="flex items-center gap-4">
+        {iconPosition === "left" && (
+          <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+        )}
+        {children}
+      </div>
+      {iconPosition === "right" && (
+        <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      )}
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
-))
-AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
+));
+AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
 
 const AccordionContent = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content> & {
+    innerClassName?: string;
+  }
+>(({ className, children, innerClassName, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
     className={cn(
       "overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down",
-      className
+      className,
     )}
     {...props}
   >
-    <div className="pb-4 pt-0">{children}</div>
+    <div className={cn("pb-4 pt-0", innerClassName)}> {children}</div>
   </AccordionPrimitive.Content>
-))
-AccordionContent.displayName = AccordionPrimitive.Content.displayName
+));
+AccordionContent.displayName = AccordionPrimitive.Content.displayName;
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
