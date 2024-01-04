5550ce5e7d4f9bb71b33384de8746ffa55216e40
Burak Saraloglu <buraksaraloglu1@gmail.com>
2024-01-04 01:39:06 +0100

    feat: holistic optimizations to import account

--- app/(app)/app/integrations/facebook/table.tsx
+++ app/(app)/app/integrations/facebook/table.tsx
@@ -26,6 +26,9 @@ import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
