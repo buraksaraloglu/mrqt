095c4e2fdf050696e7df7d5d1561b8b92c98d4ce
Burak Saraloglu <buraksaraloglu1@gmail.com>
2024-01-04 01:53:29 +0100

    feat: vercel speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 
 import { fontSans, fontUrban } from "@/assets/fonts";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
@@ -72,6 +73,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <Toaster />
             <ModalProvider />
             <TailwindIndicator />
+            <SpeedInsights />
           </InnerProviders>
         </body>
       </html>
